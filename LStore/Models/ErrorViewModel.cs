39336b776cac0610e302595f20256a942de88570
леонид сарбукоа <sarbukov.leonid@gmail.com>
2023-05-16 02:16:35 +0300

    Добавьте файлы проекта.

---
+++ LStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
